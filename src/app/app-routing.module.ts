9f0fbe62ce0e572fc4405836e13c7a29b8c7cffd
Bohdan-Balytskyy <bbalitsky@gmail.com>
2021-05-23 18:57:34 +0300

    add routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { MyPageComponent } from './my-page/my-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'my-page', component: MyPageComponent },
+  { path: 'users/:id', component: UserDetailsComponent },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { MyPageComponent } from './my-page/my-page.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    DashboardComponent,
+    LoginComponent,
+    MyPageComponent,
+    UserDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
